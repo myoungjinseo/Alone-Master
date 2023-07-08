0a79539df1e5f9b6350edf46342f27f130a05c0f
MyoungJinSeo <sih5673@gmail.com>
2023-07-08 23:16:12 +0900

    start setting

---
+++ src/main/java/com/example/alonemaster/AloneMasterApplication.java
@@ -0,0 +1,13 @@
+package com.example.alonemaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AloneMasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AloneMasterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/alonemaster/AloneMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.alonemaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AloneMasterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
